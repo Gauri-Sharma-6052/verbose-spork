7cf76bbdab08c9b82c1e9d9bf42f0980f2c44777
Gauri-Sharma-6052 <sharmagauri2506@gmail.com>
2024-07-09 22:07:36 +0530

    Add files via upload

---
+++ SIZE OF INT.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+    int arr[5];
+
+    printf("size of the array: %lu bytes\n",sizeof(arr));
+    printf("size of an integer: %lu bytes\n", sizeof(int));
+      
+
+    return 0;
+}
+
